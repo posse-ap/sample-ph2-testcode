07950547cd8bbdb9fc207d4aa472b4c2e410a018
Mitsuru Hashimoto <mitsuru_hashimoto@negocia.jp>
2021-11-17 21:35:16 +0900

    FizzBuzzクラスの中身を作成

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -1,11 +1,36 @@
 <?php
-
 // FizzBuzzクラスの抽象クラスです
-// FizzBuzzクラス内にexecuteとisMultipleOfThreeとisMultipleOfFiveを実装する必要があります
 abstract class BaseFizzBuzz {
   abstract public function execute(int $number);
   abstract protected function isMultipleOfThree(int $number): bool;
   abstract protected function isMultipleOfFive(int $number): bool;
 }
 
-class FizzBuzz extends BaseFizzBuzz {}
+class FizzBuzz extends BaseFizzBuzz {
+  public function execute(int $number)
+  {
+    if ($this->isMultipleOfThree($number) && $this->isMultipleOfFive($number)) {
+      return 'FizzBuzz';
+    }
+
+    if ($this->isMultipleOfThree($number)) {
+      return 'Fizz';
+    }
+
+    if ($this->isMultipleOfFive($number)) {
+      return 'Buzz';
+    }
+
+    return $number;
+  }
+
+  protected function isMultipleOfThree(int $number): bool
+  {
+    return $number % 3 === 0;
+  }
+
+  protected function isMultipleOfFive(int $number): bool
+  {
+    return $number % 5 === 0;
+  }
+}
